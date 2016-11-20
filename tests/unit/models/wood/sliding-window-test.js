ce575817774337b157220eca5ffd2161b9d26a7e
Farnoosh <farnoosh@pdx.edu>
2016-11-20 01:17:52 -0800

    supporting files added by ember

---
+++ tests/unit/models/wood/sliding-window-test.js
@@ -0,0 +1,12 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('wood/sliding-window', 'Unit | Model | wood/sliding window', {
+  // Specify the other units that are required for this test.
+  needs: []
+});
+
+test('it exists', function(assert) {
+  let model = this.subject();
+  // let store = this.store();
+  assert.ok(!!model);
+});
---
+++ tests/unit/routes/wood/sliding-windows-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:wood/sliding-windows', 'Unit | Route | wood/sliding windows', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
